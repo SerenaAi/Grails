464d67b2bdf4ef4d389e908ac7f7c5eb3e7efe9e
Gerardo Orellana <orellg@umn.edu>
2015-02-25 20:56:36 -0600

    Added functional test for LoginPage.

--- test/functional/org/auction/LoginPageSpec.groovy
+++ test/functional/org/auction/LoginPageSpec.groovy
@@ -1,36 +1,30 @@
 package org.auction
-
-import geb.Page
 import geb.spock.GebReportingSpec
-import pages.*
 
 class LoginPageSpec extends GebReportingSpec {
-    def "invalid login"() {
+    def "Invalid Login"() {
        given: "I am at the login page"
        to LoginPage
 
        when: "I am entering invalid password"
-       loginForm.email = "admin"
-       loginForm.password = "ioguffwf"
+       loginForm.email = "admin@yahoo.com"
+       loginForm.password = "asiaidjd"
        loginButton.click()
 
        then: "I am being redirected to the login page, the password I entered is wrong"
        at LoginPage
-       loginForm.email == "admin"
-       !loginForm.password
+       loginForm.size() == 1
     }
-    
     def "admin login"() {
        given : "I am at the login page"
        to LoginPage
 
        when: "I am entering valid username and password"
        loginForm.email = "g@g.com"
-       loginForm.password = "a111111"
+       loginForm.password = "1aaaaaaa"
        loginButton.click()
 
        then: "I am being redirected to the user homepage"
-       at LoginPage 
        $().text().contains("Show Account")
     }
 }

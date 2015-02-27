cac2030ee104e6d0c46e1c5976a0330e6077ddab
Sijia Ai <aixxx033@umn.edu>
2015-02-26 23:38:40 -0600

    r

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-auction
+MyAuction
 =======
 
 A really cool auction for the SENG class
+
+## How to Run it ##
+In order to run the app, do the following:
+
+    grails run-app
+
+## How to Test it ##
+
+    grails -Dgeb.env=chrome test-app
+
+**Note** that the functional test makes use of the `Chrome` or `Firefox` driver.
+
---
+++ test/integration/org/auction/AccountIntegrationSpec.groovy
@@ -0,0 +1,37 @@
+package org.auction
+
+import spock.lang.Specification
+
+class AccountIntegrationSpec extends Specification {
+
+    def ac 
+	def setup() {
+	}
+
+	def cleanup() {
+        ac.delete()
+	}
+
+    void "Test creating a new user account into the DB"() { 
+        given: 'Account Controller'
+        ac = new Account(name: "Peter Pan", address: "123 Some St", email: "x@x.com", password:"99912456z", type: 2).save(failOnError: true)
+
+        when: 'Creating account called'
+        ac.create()
+
+        then: 'Checking account exists'
+        ac.exists(ac.id)
+    }
+
+    void "Test deleting a new user account from the DB"() { 
+        given: 'Account Controller'
+        ac = new Account(name: "Peter Pan", address: "123 Some St", email: "x@x.com", password:"99912456z", type: 2).save(failOnError: true)
+
+        when: 'Deleting account called'
+        ac.delete()
+
+        then: 'Checking account was deleted'
+        !ac.exists(ac.id)
+    }
+
+}

01132a218a273805d6b4a1cf6df4664478f06596
Gerardo Orellana <orellg@umn.edu>
2015-04-26 23:10:18 -0500

    Added client-side validation per requirements.

--- grails-app/assets/javascripts/controllers/SigninController.js
+++ grails-app/assets/javascripts/controllers/SigninController.js
@@ -1,5 +1,24 @@
 app.controller("SigninController", function($scope, Signin, Accounts, Auth) {
     $scope.signin = function() {
+        // client validation
+        var errors = [];
+        if (!$scope.username) {
+            errors.push("Please enter your username");
+            $(".login-username").closest(".fieldcontain").addClass("has-error");
+        }
+        if (!$scope.password) {
+            errors.push("Please enter your password");
+            $(".login-password").closest(".fieldcontain").addClass("has-error");
+        }
+
+        if (errors.length) {
+            $(".my-alerts").html($("<div />")
+                .html(errors.join("<br>"))
+                .addClass("alert alert-danger login-fail")
+            );
+            return false;
+        }
+
         var data = 'j_username=' + $scope.username + '&j_password=' + $scope.password;
         Signin.check(data).$promise.then(function(data) {
             if (data.error) {
